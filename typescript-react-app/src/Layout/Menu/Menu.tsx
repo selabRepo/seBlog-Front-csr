12df0c25674597120cdd5c96e3a4b8142ba6d0d1
l2s <48622670+leehosu@users.noreply.github.com>
2019-11-03 02:35:37 +0900

    feat: keyword accessible (#13)

--- typescript-react-app/src/Layout/Menu/Menu.tsx
+++ typescript-react-app/src/Layout/Menu/Menu.tsx
@@ -4,9 +4,9 @@ import './Menu.css';
 const Menu = () => 
     <div className="App-Menu">
       <div className="Menu-container">
-        <a className="menu-item">about</a>
-        <a className="menu-item">blog</a>
-        <a className="menu-item">setting</a>
+      <a href='#/' className="menu-item" >about</a>
+      <a href='#/' className="menu-item">blog</a>
+      <a href='#/' className="menu-item">setting</a>
       </div>
     </div>
   ;
