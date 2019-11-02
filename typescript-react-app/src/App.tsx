2350a03fcc32b5f09eeb51a97fe4ff213d8ee708
leehosu <hosu4549@gmail.com>
2019-11-02 22:46:46 +0900

    feat: conflict force push

--- typescript-react-app/src/App.tsx
+++ typescript-react-app/src/App.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
-import './App.css';
+import Layout from './Layout'
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Layout />
     );
   }
 }
