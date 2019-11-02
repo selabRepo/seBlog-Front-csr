0bff1dbd60f95bbde01f7429b9f587ea776a3aea
leehosu <hosu4549@gmail.com>
2019-11-03 04:24:50 +0900

    fix : HashRouter -> BrowserRouter

--- typescript-react-app/src/Layout/Contents/Contents.tsx
+++ typescript-react-app/src/Layout/Contents/Contents.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Contents.css'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import About from '../../pages/About'
 
 const Contents = () => {
