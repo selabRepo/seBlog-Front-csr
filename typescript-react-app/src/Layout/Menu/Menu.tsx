5e3eaab3496f3ce662a0839723d5039cb5555fdc
leehosu <hosu4549@gmail.com>
2019-11-03 08:16:44 +0900

    fix : active menu styling

--- typescript-react-app/src/Layout/Menu/Menu.tsx
+++ typescript-react-app/src/Layout/Menu/Menu.tsx
@@ -6,7 +6,7 @@ const Menu = () => {
   return (
     <div className="App-Menu">
       <div className="Menu-container">
-          <NavLink to='/' className="menu-item" activeClassName='active'>Home</NavLink>
+          <NavLink exact to='/' className="menu-item" activeClassName='active'>Home</NavLink>
           <NavLink to='/About' className="menu-item" activeClassName='active'>About</NavLink>
           <NavLink to='/Blog' className="menu-item" activeClassName='active'>Blog</NavLink>
           <NavLink to='/Setting' className="menu-item" activeClassName='active'>Setting</NavLink>
