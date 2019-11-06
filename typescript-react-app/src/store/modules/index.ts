93d1ce3eb4bf746f5d32080e6f823b4316ea1a70
jyb5858 <qtrgor@gmail.com>
2019-11-06 23:58:20 +0900

    feat: blog Detail ducks

--- typescript-react-app/src/store/modules/index.ts
+++ typescript-react-app/src/store/modules/index.ts
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import blogList, { BlogListState } from "./blogList";
+import blog, { Blog } from "./blog";
 export default combineReducers({
-  blogList
+  blogList,
+  blog
 });
 
 export type RootState = {
   blogList: BlogListState;
+  blog: Blog;
 };
