322c9e306cffffe0177b4473e464c4d9a65435b4
yzzzzun <50795488+yzzzzun@users.noreply.github.com>
2019-12-08 16:39:25 +0900

    create Header, MainTitle component (#47)

--- seblog/src/Layout/Header/Header.tsx
+++ seblog/src/Layout/Header/Header.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Header.scss";
+import MainTitle from '../../Components/MainTitle';
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header__wrap">
-      <div className="header__box">
+      <header className="header__box">
         <div className="logo__wrap">
           <div className="logo__box">
+              <img className="logo logo--main_selog" src="/public/img/SELAB_logo.png" alt="logo" />
             <Link className="logo--main_selog" to="/">
               selog
             </Link>
@@ -16,10 +18,11 @@ const Header = () => {
         <div className="button__wrap--header">
           <div className="button__box">
             <button className="button button--menu"></button>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+      </header> 
+      <MainTitle mainTitle="SE.LOG" subTitle={["Software Engineering LAB",<br></br>,"Technology Blog"]}></MainTitle>
+    </div>   
   );
 };
 export default Header;
