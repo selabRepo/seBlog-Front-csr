e488389a1374f97182a6c2460592134c0c4f825d
l2s <48622670+leehosu@users.noreply.github.com>
2019-12-15 16:41:33 +0900

    update a route and link to logo, maintitle refactoring (#55)
    *  update a route and link to logo, maintitle refactoring
    
    * main title props about header

--- seblog/src/Layout/Header/Header.tsx
+++ seblog/src/Layout/Header/Header.tsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Header.scss";
-import MainTitle from '../../Components/MainTitle';
+import MainTitle from "../../Components/MainTitle";
 import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <div className="header__wrap">
-      <header className="header__box">
-        <div className="logo__wrap">
-          <div className="logo__box">
-              <img className="logo logo--main_selog" src="/public/img/SELAB_logo.png" alt="logo" />
-            <Link className="logo--main_selog" to="/">
-              selog
-            </Link>
-          </div>
-        </div>
-        <div className="button__wrap--header">
-          <div className="button__box">
-            <button className="button button--menu"></button>
+type titleTypes = {
+  mainTitle: string;
+  subTitle: string;
+};
+
+class Header extends Component {
+  state: titleTypes = {
+    mainTitle: "SE.LOG",
+    subTitle: "Software Engineering LAB Technology Blog"
+  };
+
+  render() {
+    return (
+      <div className="header__wrap">
+        <header className="header__box">
+          <div className="logo__wrap">
+            <div className="logo__box">
+              <Link className="logo--main_selog" to="/">
+                selog
+              </Link>
             </div>
           </div>
-      </header> 
-      <MainTitle mainTitle="SE.LOG" subTitle={["Software Engineering LAB",<br></br>,"Technology Blog"]}></MainTitle>
-    </div>   
-  );
-};
+          <div className="button__wrap--header">
+            <div className="button__box">
+              <button className="button button--menu"></button>
+            </div>
+          </div>
+        </header>
+        <MainTitle
+          mainTitle={this.state.mainTitle}
+          subTitle={this.state.subTitle}
+        />
+      </div>
+    );
+  }
+}
 export default Header;
